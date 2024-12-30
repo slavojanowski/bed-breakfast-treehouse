497cd1c873d8bdd1a33095382af1514306458fc1
slavojanowski <slavojanowski@gmail.com>
2024-12-30 23:55:42 +0100

    Main slider edition

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,16 @@ import "./App.css";
 import Home from "./components/pages/Home";
 import Create from "./components/pages/Create";
 import Update from "./components/pages/Update";
+import MainSlider from "./components/main-slider/MainSlider";
+
+// SLIDER
+import slideImg1 from "./components/main-slider/images/car-1.jpg";
+import slideImg2 from "./components/main-slider/images/car-2.jpg";
+import slideImg3 from "./components/main-slider/images/car-3.jpg";
+import slideImg4 from "./components/main-slider/images/car-4.jpg";
+import slideImg5 from "./components/main-slider/images/car-5.jpg";
+
+const IMAGES = [slideImg1, slideImg2, slideImg3, slideImg4, slideImg5];
 
 function App() {
   return (
@@ -15,6 +25,10 @@ function App() {
           <Link to="/">Home</Link>
           <Link to="/create">Create New Smoothie</Link>
         </nav>
+        <div className="slider-container">
+          <MainSlider imageUrls={IMAGES} />
+        </div>
+
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/create" element={<Create />} />
