36a1d4968cd07fe4b5d135ef08b929acc332659b
slavojanowski <slavojanowski@gmail.com>
2025-01-16 15:06:20 +0100

    Weather App edit

--- src/components/pages/user-account-page/UserAccount.jsx
+++ src/components/pages/user-account-page/UserAccount.jsx
@@ -8,13 +8,13 @@ import ButtonLarge from "../../global/ButtonLarge";
 import "../../bookings/css/booked-room-details-tile.css";
 import "./css/user-account-page.css";
 import { useNavigate } from "react-router";
-import { CSSTransition, TransitionGroup } from "react-transition-group";
+import WeatherForecast from "../../weather-forecast/WeatherForecast";
 
 const UserAccount = () => {
   const [fetchError, setFetchError] = useState(null);
   const [bookings, setBookings] = useState(null);
   const [userName, setUserName] = useState(null);
-  const [activeSection, setActiveSection] = useState("");
+  const [activeSection, setActiveSection] = useState("weatherParam");
   const navigate = useNavigate();
 
   const handleDelete = (id) => {
@@ -83,162 +83,99 @@ const UserAccount = () => {
         </PageBanner>
       </PageCover>
 
-      <TransitionGroup>
-        <>
-          <div className="page user-account-page">
-            <section className="acc-heading">
-              <div
-                onClick={() => handleColumnClick("bookingsParam")}
-                className="column bookings-info"
-              >
-                <h4>Historia Twoich rezerwacji</h4>
-              </div>
-              <div
-                onClick={() => handleColumnClick("todoParam")}
-                className="column todo-info"
-              >
-                <h4>Zaplanuj swój pobyt</h4>
-              </div>
-              <div
-                onClick={() => handleColumnClick("weatherParam")}
-                className="column weather-info"
-              >
-                <h4>Sprawdź lokalną pogodę</h4>
-              </div>
-              <div className="column log-info">
-                <h5>
-                  Witaj <b>{userName ? userName : "User"}</b>, jesteś
-                  zalogowany.
-                </h5>
-                <button onClick={signOut}>Wyloguj się</button>
-              </div>
-            </section>
+      <div className="page user-account-page">
+        <section className="acc-heading">
+          <div
+            onClick={() => handleColumnClick("bookingsParam")}
+            className={`column bookings-info ${
+              activeSection === "bookingsParam" ? "active-elem" : ""
+            }`}
+          >
+            <h4>Historia Twoich rezerwacji</h4>
+          </div>
+          <div
+            onClick={() => handleColumnClick("todoParam")}
+            className={`column todo-info ${
+              activeSection === "todoParam" ? "active-elem" : ""
+            }`}
+          >
+            <h4>Zaplanuj swój pobyt</h4>
+          </div>
+          <div
+            onClick={() => handleColumnClick("weatherParam")}
+            className={`column weather-info ${
+              activeSection === "weatherParam" ? "active-elem" : ""
+            }`}
+          >
+            <h4>Sprawdź lokalną pogodę</h4>
+          </div>
+          <div className="column log-info">
+            <h5>
+              Witaj <b>{userName ? userName : "User"}</b>, jesteś zalogowany.
+            </h5>
+            <button onClick={signOut}>Wyloguj się</button>
+          </div>
+        </section>
 
-            <div className="acc-container">
-              {/* ----- bookings section ----- */}
-              {activeSection === "bookingsParam" && (
-                <div className="section bookings-section">
-                  {fetchError && <p>{fetchError}</p>}
-                  {bookings && (
-                    <div className="bookings">
-                      <div className="booking-grid">
-                        {bookings.map((booking, index) => (
-                          <CSSTransition
-                            key={"csst" + index}
-                            in={true}
-                            appear={true}
-                            timeout={2500}
-                            onEnter={() => console.log("onEnter")}
-                            onEntering={() => console.log("onEntering")}
-                            onEntered={() => console.log("onEntered")}
-                            onExit={() => console.log("onExit")}
-                            onExiting={() => console.log("onExiting")}
-                            onExited={() => console.log("onExited")}
-                            classNames="css-transition"
-                          >
-                            <>
-                              <BookingCard
-                                key={booking.id}
-                                booking={booking}
-                                onDelete={handleDelete}
-                              />
-                            </>
-                          </CSSTransition>
-                        ))}
-                      </div>
-                    </div>
-                  )}
+        <div className="acc-container">
+          {/* ----- bookings section ----- */}
+          {activeSection === "bookingsParam" && (
+            <div className="section bookings-section">
+              {fetchError && <p>{fetchError}</p>}
+              {bookings && (
+                <div className="bookings">
+                  <div className="booking-grid">
+                    {bookings.map((booking) => (
+                      <BookingCard
+                        key={booking.id}
+                        booking={booking}
+                        onDelete={handleDelete}
+                      />
+                    ))}
+                  </div>
                 </div>
               )}
-
-              {/* ----- plan your stay section ----- */}
-              {activeSection === "todoParam" && (
-                <CSSTransition
-                  in={true}
-                  appear={true}
-                  timeout={2500}
-                  onEnter={() => console.log("onEnter")}
-                  onEntering={() => console.log("onEntering")}
-                  onEntered={() => console.log("onEntered")}
-                  onExit={() => console.log("onExit")}
-                  onExiting={() => console.log("onExiting")}
-                  onExited={() => console.log("onExited")}
-                  classNames="css-transition"
-                >
-                  <div className="section todo-section">
-                    The standard Lorem Ipsum passage, used since the 1500s
-                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                    sed do eiusmod tempor incididunt ut labore et dolore magna
-                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                    Duis aute irure dolor in reprehenderit in voluptate velit
-                    esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-                    occaecat cupidatat non proident, sunt in culpa qui officia
-                    deserunt mollit anim id est laborum." Section 1.10.32 of "de
-                    Finibus Bonorum et Malorum", written by Cicero in 45 BC "Sed
-                    ut perspiciatis unde omnis iste natus error sit voluptatem
-                    accusantium doloremque laudantium, totam rem aperiam, eaque
-                    ipsa quae ab illo inventore veritatis et quasi architecto
-                    beatae vitae dicta sunt explicabo. Nemo enim ipsam
-                    voluptatem quia voluptas sit aspernatur aut odit aut fugit,
-                    sed quia consequuntur magni dolores eos qui ratione
-                    voluptatem sequi nesciunt. Neque porro quisquam est, qui
-                    dolorem ipsum quia dolor sit amet, consectetur, adipisci
-                    velit, sed quia non numquam eius modi tempora incidunt ut
-                    labore et dolore magnam aliquam quaerat voluptatem. Ut enim
-                    ad minima veniam, quis nostrum exercitationem ullam corporis
-                    suscipit laboriosam, nisi ut aliquid ex ea commodi
-                    consequatur? Quis autem vel eum iure reprehenderit qui in ea
-                    voluptate velit esse quam nihil molestiae consequatur, vel
-                    illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
-                    1914 translation by H. Rackham.
-                  </div>
-                </CSSTransition>
-              )}
-
-              {/* ----- check local weather section ----- */}
-              {activeSection === "weatherParam" && (
-                <CSSTransition
-                  in={true}
-                  appear={true}
-                  timeout={2500}
-                  onEnter={() => console.log("onEnter")}
-                  onEntering={() => console.log("onEntering")}
-                  onEntered={() => console.log("onEntered")}
-                  onExit={() => console.log("onExit")}
-                  onExiting={() => console.log("onExiting")}
-                  onExited={() => console.log("onExited")}
-                  classNames="css-transition"
-                >
-                  <div className="section weather-section">
-                    Pleasure and praising pain was born and I will give you a
-                    complete account of the system, and expound the actual
-                    teachings of the great explorer of the truth, the
-                    master-builder of human happiness. No one rejects, dislikes,
-                    or avoids pleasure itself, because it is pleasure, but
-                    because those who do not know how to pursue pleasure
-                    rationally encounter consequences that are extremely
-                    painful. Nor again is there anyone who loves or pursues or
-                    desires to obtain pain of itself, because it is pain, but
-                    because occasionally circumstances occur in which toil and
-                    pain can procure him some great pleasure. To take a trivial
-                    example, which of us ever undertakes laborious physical
-                    exercise, except to obtain some advantage from it? But who
-                    has any right to find fault with a man who chooses to enjoy
-                    a pleasure that has no annoying consequences, or one who
-                    avoids a pain that produces no resultant pleasure?" Section
-                    1.10.33 of "de Finibus Bonorum et Malorum", written by
-                    Cicero in 45 BC "At vero eos et accusamus et iusto odio
-                    dignissimos ducimus qui blanditiis praesentium voluptatum
-                    deleniti atque corrupti quos dolores et quas molestias
-                    excepturi sint occaecati cupiditate
-                  </div>
-                </CSSTransition>
-              )}
             </div>
-          </div>
-        </>
-      </TransitionGroup>
+          )}
+
+          {/* ----- plan your stay section ----- */}
+          {activeSection === "todoParam" && (
+            <div className="section todo-section">
+              The standard Lorem Ipsum passage, used since the 1500s "Lorem
+              ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+              tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+              minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+              aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum."
+              Section 1.10.32 of "de Finibus Bonorum et Malorum", written by
+              Cicero in 45 BC "Sed ut perspiciatis unde omnis iste natus error
+              sit voluptatem accusantium doloremque laudantium, totam rem
+              aperiam, eaque ipsa quae ab illo inventore veritatis et quasi
+              architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam
+              voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed
+              quia consequuntur magni dolores eos qui ratione voluptatem sequi
+              nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor
+              sit amet, consectetur, adipisci velit, sed quia non numquam eius
+              modi tempora incidunt ut labore et dolore magnam aliquam quaerat
+              voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem
+              ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi
+              consequatur? Quis autem vel eum iure reprehenderit qui in ea
+              voluptate velit esse quam nihil molestiae consequatur, vel illum
+              qui dolorem eum fugiat quo voluptas nulla pariatur?" 1914
+              translation by H. Rackham.
+            </div>
+          )}
+
+          {/* ----- check local weather section ----- */}
+          {activeSection === "weatherParam" && (
+            <div className="section weather-section">
+              <WeatherForecast />
+            </div>
+          )}
+        </div>
+      </div>
     </>
   );
 };
---
+++ src/components/weather-forecast/WeatherForecast.jsx
@@ -0,0 +1,124 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import "./css/weather-forecast.css";
+import searchIcon from "/weather-images/search.png";
+import clearIcon from "/weather-images/clear.png";
+import cloudIcon from "/weather-images/cloud.png";
+import drizzleIcon from "/weather-images/drizzle.png";
+import humidityIcon from "/weather-images/humidity.png";
+import rainIcon from "/weather-images/rain.png";
+import snowIcon from "/weather-images/snow.png";
+import windIcon from "/weather-images/wind.png";
+
+const WeatherForecast = () => {
+  const [city, setCity] = useState("Ełk");
+  const [weatherData, setWeatherData] = useState(null);
+  const [error, setError] = useState("");
+
+  const API_KEY = "add2852ec067e64922026db2f029112f";
+
+  const fetchWeatherData = async (cityName) => {
+    try {
+      const response = await axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${API_KEY}`
+      );
+
+      const allIcons = {
+        "01d": clearIcon,
+        "01n": clearIcon,
+        "02d": cloudIcon,
+        "02n": cloudIcon,
+        "03d": cloudIcon,
+        "03n": cloudIcon,
+        "04d": drizzleIcon,
+        "04n": drizzleIcon,
+        "09d": rainIcon,
+        "09n": rainIcon,
+        "10d": rainIcon,
+        "10n": rainIcon,
+        "13d": snowIcon,
+        "13n": snowIcon,
+      };
+
+      const icon = allIcons[response.data.weather[0].icon] || clearIcon;
+
+      setWeatherData({ ...response.data, icon });
+      setError("");
+    } catch (error) {
+      console.log("Nie można pobrać informacji", error);
+
+      setError("Nie znaleziono takiego miasta");
+      setWeatherData(null);
+    }
+  };
+
+  useEffect(() => {
+    fetchWeatherData(city);
+  }, []);
+
+  const handleSearch = (event) => {
+    event.preventDefault();
+    const cityInput = event.target.elements.city.value.trim();
+    if (cityInput) {
+      fetchWeatherData(cityInput);
+    }
+    event.target.elements.city.value = "";
+  };
+
+  return (
+    <>
+      <div className="weather-parent-grid">
+        <div className="weather">
+          <div className="search-bar">
+            <form onSubmit={handleSearch}>
+              <input
+                name="city"
+                type="text"
+                placeholder="Wpisz nazwę miasta"
+                className="form-control"
+              />
+              <button type="submit">
+                <img src={searchIcon} alt="search icon" />
+              </button>
+            </form>
+          </div>
+
+          {error && <p className="error">{error}</p>}
+
+          {weatherData && weatherData.main ? (
+            <>
+              <img
+                src={weatherData.icon}
+                alt="weather icon"
+                className="weather-icon"
+              />
+              <p className="temperature">{weatherData.main.temp}℃</p>
+              <p className="location">{weatherData.name}</p>
+              <div className="weather-data">
+                <div className="col">
+                  <img src={humidityIcon} alt="humidity icon" />
+                  <div>
+                    <p>{weatherData.main.humidity}%</p>
+                    <span>Humidity</span>
+                  </div>
+                </div>
+
+                <div className="col">
+                  <img src={windIcon} alt="wind icon" />
+                  <div>
+                    <p>{weatherData.wind.speed} Km/h</p>
+                    <span>Wind</span>
+                  </div>
+                </div>
+              </div>
+            </>
+          ) : (
+            <p>Loading...</p>
+          )}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default WeatherForecast;
